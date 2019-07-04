ec01ac32c8e9e91b9b7c5c6a3d370529e6dcd1a1
Anas Al-Masri <31314792+AnasAlmasri@users.noreply.github.com>
2019-07-04 14:55:59 +0800

    added results table and image headers

--- static/js/default.js
+++ static/js/default.js
@@ -1,3 +1,34 @@
+//Django basic setup for accepting ajax requests.
+// Cookie obtainer Django
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+var csrftoken = getCookie('csrftoken');
+// Setup ajax connections safetly
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
 
 
 $(function() {
@@ -6,19 +37,38 @@ $(function() {
         var uploadFile = $(this);
         var files = !!this.files ? this.files : [];
         if (!files.length || !window.FileReader) return; // no file selected, or no FileReader support
-
+        
         if (/^image/.test( files[0].type)){ // only image file
             var reader = new FileReader(); // instance of the FileReader
             reader.readAsDataURL(files[0]); // read the local file
 
             reader.onloadend = function(){ // set image data as background of div
                 //alert(uploadFile.closest(".upimage").find('.imagePreview').length);
-                uploadFile.closest(".imgUp").find('.imagePreview').css("background-image", "url("+this.result+")");
+                uploadFile.closest(".imgUp").find('.imagePreview').css("background-image", "url("+this.result+")");  
+                $.ajax({
+                    url: 'data_ajax_request',
+                    data: {
+                        'message': 'I want an AJAX response',
+                        'img_data': this.result
+                    },
+                    dataType: 'json',
+                    type: 'POST',
+                    success: function(data) {
+                        if (data.is_valid) {
+                            console.log(data.response);
+                        } else {
+                            console.log("You didn't message : I want an AJAX response");
+                        }
+                    }
+                });
             }
+            
+            
         }
     
     });
 });
+
 
 
 
