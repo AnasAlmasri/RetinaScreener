ce4d40dc990717d01a89d39cff5e8ebe68e57878
Anas Al-Masri <31314792+AnasAlmasri@users.noreply.github.com>
2019-07-19 05:16:18 +0800

    added patient recording form to homepage - not linked to backend

--- static/js/default.js
+++ static/js/default.js
@@ -97,12 +97,6 @@ $('#algoTypePicker').on('change', function() {
     Inputs:  self - default object
              fundus - image to segment [NumPy ndarray with 3 layers, BGR]
     Outputs: `+x+` - features [Numpy ndarray with 1 layer, BW]
-
-    Notes:
-        - Make sure not to tamper with or modify 'self'
-        - Try not to change the function definition and the return statement
-        - You can find a full list of available libraries and their versions
-        - Remember that you can define nested functions in python
     """
 
     # your code goes here
